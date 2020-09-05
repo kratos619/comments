a682866806a925bd7e0b6c031ec2b86f960c4558
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-05 19:49:59 +0530

    :rocket: start project

--- extension.js
+++ extension.js
@@ -1,14 +1,6 @@
 "use strit"
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
 	let insertInlineComment = vscode.commands.registerCommand('comment.inlineComment',() =>{
     const editor = vscode.window.activeTextEditor;
